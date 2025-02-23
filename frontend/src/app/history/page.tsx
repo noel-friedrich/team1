38359739099e90a42a85b1122c9b2510e96cba7b
George Levins <82120984+georgelevins@users.noreply.github.com>
2025-02-23 12:58:32 +0000

    like and dislike

--- frontend/src/app/history/page.tsx
+++ frontend/src/app/history/page.tsx
@@ -69,22 +69,9 @@ export default async function HistoryPage({
               <li>
                 <RandomArticleLink />
               </li>
-            </ul>
-          </div>
-          <div>
-            <div className="font-medium mb-2 text-[#54595d]">Contribute</div>
-            <ul className="space-y-1">
               <li>
                 <Link
-                  href="#"
-                  className="text-[#36c] hover:text-[#447ff5] block py-1"
-                >
-                  Help
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
+                  href="/about"
                   className="text-[#36c] hover:text-[#447ff5] block py-1"
                 >
                   About
