7c8903f11e8fcf2ef06c6689c6d9b8c2c2e059ad
Josh Wentzien <wentzien@cs.washington.edu>
2025-02-23 11:56:12 +0000

    better streaming and local

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import cv2
 from manager import Manager
 from secret_keys import TWITCH_KEY
 from video import VideoStreamer
@@ -5,21 +6,28 @@ from video import VideoStreamer
 
 def main():
     man = Manager("gpt-4o-mini", upload_online=True)
-    stream_video = False
-    if not stream_video:
+    VIDEO_MODE = True
+    if not VIDEO_MODE:
         NUM_ITRS = 30
         for _ in range(NUM_ITRS):
             man.get_next_article()
     else:
+        STREAM_MODE = False
         # Replace with your actual streaming URL and stream key.
         STREAM_URL = f"rtmp://live.twitch.tv/app/{TWITCH_KEY}"
-        streamer = VideoStreamer(stream_url=STREAM_URL)
+        if STREAM_MODE:
+            streamer = VideoStreamer(
+                width=480, height=360, fps=30, stream_mode=True, stream_url=STREAM_URL
+            )
+        else:
+            # cv2.namedWindow("Live Stream", cv2.WINDOW_NORMAL)
+            streamer = VideoStreamer(width=1920, height=1080, fps=60, stream_mode=False)
         try:
             while True:
                 article = man.get_next_article()
                 ai_text = f"{article.title}\n\n{article.content}"
                 print("Displaying text:", ai_text)
-                streamer.stream_text(ai_text, typing_delay=0.005)
+                streamer.stream_text(ai_text, typing_delay=0.0005)
                 streamer.display_black_screen()
         except KeyboardInterrupt:
             print("Live stream stopped.")
